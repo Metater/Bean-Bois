1acc365378878006e6b7a744b3bb20e3a5dc71e7
Connor <61102529+Metater@users.noreply.github.com>
2022-11-25 11:42:31 -0500

    Stuff

---
+++ Assets/Scripts/MaterialScroll.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MaterialScroll : MonoBehaviour
+{
+    [SerializeField] private Material scrollMaterial;
+    [SerializeField] private Vector2 scrollVelocity;
+
+    private Vector2 scrollPosition = Vector2.zero;
+
+    private void Update()
+    {
+        scrollMaterial.mainTextureOffset = scrollPosition;
+        scrollPosition += scrollVelocity * Time.deltaTime;
+    }
+}
